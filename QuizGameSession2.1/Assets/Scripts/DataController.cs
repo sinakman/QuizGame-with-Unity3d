dcfc7b8648ec79ef552ece99a764c99d095a85c5
sinakman <snakmn@gmail.com>
2019-10-29 01:15:29 +0300

    Quiz Game with Unity
    True Or False Game. Play on multiple platforms.

---
+++ QuizGameSession2.1/Assets/Scripts/DataController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.IO;
+using System.Text;
+using System;
+
+public class DataController : MonoBehaviour
+{
+    private RoundData[] allRoundData;
+
+    private PlayerProgress playerProgress;
+    private string gameDataFileName =  "data3.txt";
+
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        LoadGameData();
+        LoadPlayerProgress();
+
+        SceneManager.LoadScene("MenuScreen");
+    }
+
+    public RoundData GetCurrentRoundData()
+    {
+        return allRoundData[0];
+    }
+
+    public void SubmitNewPlayerScore(int newScore)
+    {
+        if (newScore > playerProgress.highestScore)
+        {
+            playerProgress.highestScore = newScore;
+            SavePlayerProgress();
+        }
+    }
+
+    public int GetHighestPlayerScore()
+    {
+        return playerProgress.highestScore;
+    }
+
+
+    private void LoadPlayerProgress()
+    {
+        playerProgress = new PlayerProgress();
+
+        if (PlayerPrefs.HasKey("highestScore"))
+        {
+            playerProgress.highestScore = PlayerPrefs.GetInt("highestScore");
+        }
+    }
+
+    private void SavePlayerProgress()
+    {
+        PlayerPrefs.SetInt("highestScore", playerProgress.highestScore);
+    }
+
+    private void LoadGameData()
+    {
+
+
+
+        string filePath = Path.Combine(Application.streamingAssetsPath, gameDataFileName);
+
+        if (File.Exists(filePath))
+        {
+
+
+           
+
+            string dataAsJson = File.ReadAllText(filePath);
+            GameData loadedData = JsonUtility.FromJson<GameData>(dataAsJson);
+            allRoundData = loadedData.allRoundData;
+
+
+        }
+        else
+        {
+            Debug.LogError("Cannot load game data!");
+        }
+    }
+
+}
---
+++ QuizGameSession2.1/Assets/Scripts/SFXPlayin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPlayin : MonoBehaviour
+{
+    public AudioSource clicksound;
+    public AudioSource incorrect;
+    public AudioSource correct;
+
+    public void PlayClickSound()
+    {
+        clicksound.Play();
+    }
+
+    public void EndRoundSound()
+    {
+        incorrect.Play();
+    }
+
+    public void TrueRoundSound()
+    {
+        correct.Play();
+    }
+}
